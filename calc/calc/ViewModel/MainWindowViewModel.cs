b64abdbcd646159af9f3520001b4782f823f34c0
Shin hyo seop <hsshinplas@icloud.com>
2018-12-17 17:39:00 +0900

    기본 UI, Viewmodel 생성

---
+++ calc/calc/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calc.ViewModel
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private const string TAG = "[MainWindow]";
+
+        private double _accu;
+        private double _res;
+        private int _oper;
+
+        private enum state
+        {
+            LIT, OPER, DONE, NONE,
+        }
+
+        private enum operEnum
+        {
+            PLUS, MINUS, MUL, DIV, PERCENT, SQRT, SQR, EQ, DOT, PM, C, CE, BACK, REVERSE,
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnpropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
