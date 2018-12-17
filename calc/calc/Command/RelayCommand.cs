7e5e9454c87bea438069a654c78497e98ddb0e60
Shin hyo seop <hsshinplas@icloud.com>
2018-12-17 17:50:46 +0900

    뷰모델 작성중, 버튼 커맨드 추가

---
+++ calc/calc/Command/RelayCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace calc.Command
+{
+    class RelayCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        private Action<object> _action;
+
+        public RelayCommand(Action<object> action)
+        {
+            _action = action;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if(parameter != null)
+            {
+                _action(parameter);
+            }
+            else
+            {
+                _action(null);
+            }
+        }
+    }
+}
--- calc/calc/ViewModel/MainWindowViewModel.cs
+++ calc/calc/ViewModel/MainWindowViewModel.cs
@@ -1,19 +1,68 @@
-﻿using System;
+﻿using calc.Command;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace calc.ViewModel
 {
     class MainWindowViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnpropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
         private const string TAG = "[MainWindow]";
 
         private double _accu;
         private double _res;
         private int _oper;
+
+        private string _historyString;
+
+        public ICommand buttonCommand { get; set; }
+
+        public double accu
+        {
+            get { return _accu; }
+            set
+            {
+                _accu = value;
+                OnpropertyChanged("accu");
+            }
+        }
+        public double res
+        {
+            get { return _res; }
+            set
+            {
+                _res = value;
+                OnpropertyChanged("res");
+            }
+        }
+        public int oper
+        {
+            get { return _oper; }
+            set
+            {
+                _oper = value;
+                OnpropertyChanged("oper");
+            }
+        }
+        public string historyString
+        {
+            get { return _historyString; }
+            set
+            {
+                _historyString = value;
+                OnpropertyChanged("historyString");
+            }
+        }
 
         private enum state
         {
@@ -25,10 +74,15 @@ namespace calc.ViewModel
             PLUS, MINUS, MUL, DIV, PERCENT, SQRT, SQR, EQ, DOT, PM, C, CE, BACK, REVERSE,
         }
 
-        public event PropertyChangedEventHandler PropertyChanged;
-        protected void OnpropertyChanged(string name)
+        // constructor
+        public MainWindowViewModel()
         {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+            buttonCommand = new RelayCommand(new Action<object>(buttonClicked));
+        }
+
+        private void buttonClicked(object param)
+        {
+
         }
     }
 }
